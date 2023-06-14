006f45a97e1c37365e06a745a4dcd8e4aed6daae
Dom Sinclair <dom@viewtolearn.co.uk>
2023-06-14 14:45:48 +0100

    Add project files.

---
+++ OrdersDatabase/dbo/Tables/Orders.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Order]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [CustomerId] INT NOT NULL, 
+    [OrderDate] DATE NOT NULL, 
+    [OrderNumber] NCHAR(10) NULL, 
+    CONSTRAINT [FK_Order_Customer] FOREIGN KEY ([CustomerId]) REFERENCES [Customer]([Id])
+)
